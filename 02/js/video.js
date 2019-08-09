9fcb1f4b0310af486a72ed1aa606541c6246f0ef
Ish <ishdesigned@gmail.com>
2019-08-09 14:55:28 -0400

    added 02
    testing new portfolio site

---
+++ 02/js/video.js
@@ -0,0 +1,152 @@
+   var video1 = document.getElementById("video1");
+   video1.controls = false;
+
+     function togglePlayPause1() {
+     var playpause = document.getElementById("playpause");
+     if (video1.paused || video1.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video1.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video1.pause();
+     }
+  };
+
+  var video2 = document.getElementById("video2");
+   video2.controls = false;
+
+     function togglePlayPause2() {
+     var playpause = document.getElementById("playpause2");
+     if (video2.paused || video2.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video2.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video2.pause();
+     }
+  };
+
+  var video3 = document.getElementById("video3");
+   video3.controls = false;
+
+     function togglePlayPause3() {
+     var playpause = document.getElementById("playpause3");
+     if (video3.paused || video3.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video3.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video3.pause();
+     }
+  };
+
+  var video4 = document.getElementById("video4");
+   video4.controls = false;
+
+     function togglePlayPause4() {
+     var playpause = document.getElementById("playpause4");
+     if (video4.paused || video4.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video4.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video4.pause();
+     }
+  };
+
+  var video5 = document.getElementById("video5");
+   video5.controls = false;
+
+     function togglePlayPause5() {
+     var playpause = document.getElementById("playpause5");
+     if (video5.paused || video5.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video5.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video5.pause();
+     }
+  };
+
+  var video6 = document.getElementById("video6");
+   video6.controls = false;
+
+     function togglePlayPause6() {
+     var playpause = document.getElementById("playpause6");
+     if (video6.paused || video6.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video6.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video6.pause();
+     }
+  };
+
+  var video7 = document.getElementById("video7");
+   video7.controls = false;
+
+     function togglePlayPause7() {
+     var playpause = document.getElementById("playpause7");
+     if (video7.paused || video7.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video7.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video7.pause();
+     }
+  };
+
+  var video8 = document.getElementById("video8");
+   video8.controls = false;
+
+     function togglePlayPause8() {
+     var playpause = document.getElementById("playpause8");
+     if (video8.paused || video8.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video8.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video8.pause();
+     }
+  };
+
+  var video9 = document.getElementById("video9");
+   video9.controls = false;
+
+     function togglePlayPause9() {
+     var playpause = document.getElementById("playpause9");
+     if (video9.paused || video9.ended) {
+        playpause.title = "&#xf28b;";
+        playpause.innerHTML = "&#xf28b;";
+        video9.play();
+     }
+     else {
+        playpause.title = "&#xf144;";
+        playpause.innerHTML = "&#xf144;";
+        video9.pause();
+     }
+  };
